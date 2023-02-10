6c8ca55cd076d60fcdf60eb5efa35259fa20a0cc
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-10 10:20:27 -0600

    comment out lines not in use yet. listening on server 5001 works A4 npm start --prefix backend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 const express = require('express'); // Import express web application framework
 const dotenv = require('dotenv'); // zero dependency module to read environment variables from .env file into process.env
-const connectDB = require('./config/db')
-const logger = require('./utils/logger')
-const user = require('./routes/user');
-const bodyParser = require('body-parser')
+// const connectDB = require('./config/db')
+// const logger = require('./utils/logger')
+// const user = require('./routes/user');
+// const bodyParser = require('body-parser')
 
-dotenv.config({ path: './config/config.env' })
+// dotenv.config({ path: './config/config.env' })
 
-connectDB(); 
+// connectDB(); 
 
 const app = express(); //initialize express app
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5001; //creating a port. Read from config folder, if not then use 5001
 
-app.use(logger)
-app.use('/user', user)
+// app.use(logger)
+// app.use('/user', user)
 
 const server = app.listen(PORT, () => { //initialize server application to run
     console.log(`Server is listening on PORT ${PORT}`) //confirmation message
