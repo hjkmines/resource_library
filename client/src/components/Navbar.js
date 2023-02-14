976c4c3f2de62cd5a5a44b87b83d8d0f445f8e9a
Thomas Bell <thomasjbell065@gmail.com>
2023-02-14 00:24:58 -0500

    Updated Navbar
    added Tab layout to get some ideas and see how chakra ui works

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,37 @@
-
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Image } from '@chakra-ui/react';
 
 const Navbar = () => {
   return (
-    <div>Navbar Here...</div>
-  )
+      <Tabs isFitted variant='enclosed unstyled' size='lg'>
+          <Image boxSize='100px' fit='cover' src='/logo.svg' />
+          <TabList>
+              <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Videos</Tab>
+              <Tab _selected={{ color: 'white', bg: 'green.400' }}>
+                  Articles
+              </Tab>
+              <Tab _selected={{ color: 'white', bg: 'yellow.300' }}>Tools</Tab>
+              <Tab _selected={{ color: 'white', bg: 'red.400' }}>Events</Tab>
+              <Tab _selected={{ color: 'white', bg: 'pink.400' }}>Humour</Tab>
+          </TabList>
+          <TabPanels>
+              <TabPanel>
+                  <p>one!</p>
+              </TabPanel>
+              <TabPanel>
+                  <p>two!</p>
+              </TabPanel>
+              <TabPanel>
+                  <p>three!</p>
+              </TabPanel>
+              <TabPanel>
+                  <p>four!</p>
+              </TabPanel>
+              <TabPanel>
+                  <p>five!</p>
+              </TabPanel>
+          </TabPanels>
+      </Tabs>
+  );
 }
 
-export default Navbar
+export default Navbar;
