e36b17d311615c0dc4cef3d941ce9de77af45f71
Thomas Bell <thomasjbell065@gmail.com>
2023-02-22 21:08:37 -0500

    updated code
    added temporary cards to view

--- client/src/components/featured-section/FeaturedVideos.jsx
+++ client/src/components/featured-section/FeaturedVideos.jsx
@@ -9,169 +9,194 @@ import {
     Text,
     SimpleGrid,
     useColorModeValue,
+    Flex,
+    Spacer,
 } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 const FeaturedVideos = () => {
-    const trailerUrl = [
-        'https://www.youtube-nocookie.com/embed/Ixq-y_r-lkE',
-        'https://youtu.be/anZtoPo2Fpw',
-    ];
+    const ReactPlayer = dynamic(() => import('react-player/youtube'), {
+        ssr: false,
+    });
+
+    const trailer1 = 'https://www.youtube-nocookie.com/embed/Ixq-y_r-lkE';
+
+    const trailer2 = 'https://youtu.be/2Z4m4lnjxkY';
+
+    const trailer3 = 'https://youtu.be/anZtoPo2Fpw';
+
+    const trailer4 =
+        'https://youtu.be/5mpqhtK0NuU?list=PLCg4BmjNch8hKt_6BzvThMe-njUhFEM02';
 
     return (
-        <SimpleGrid
-            templateColumns='repeat(auto-fill, minmax(400px, 1fr))'
-            ml={{ base: '0', md: '10', lg: '20' }}
-            mt={{ base: '10', md: '10' }}
-        >
-            <Card
-                my={5}
-                maxW='sm'
-                rounded={'sm'}
-                border={'1px'}
-                borderColor='black'
-                boxShadow={useColorModeValue(
-                    '6px 6px 0 black',
-                    '6px 6px 0 cyan'
-                )}
+        <>
+            <SimpleGrid
+                templateColumns='repeat(auto-fill, minmax(400px, 1fr))'
+                ml={{ base: '20', sm: '10', md: '3', lg: '10' }}
+                mt={{ base: '10', md: '10' }}
+                align='center'
             >
-                <CardBody>
-                    <Container maxWidth='400px'>
-                        <AspectRatio ratio={4 / 3}>
-                            <ReactPlayer
-                                url={trailerUrl}
-                                width='100%'
-                                height='100%'
-                                controls={false}
-                                volume={0}
-                                muted={true}
-                                playing={false}
-                            />
-                        </AspectRatio>
-                    </Container>
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Dune (mmorpg)</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Aspernatur facere, alias laudantium
-                            voluptatibus ab veniam cum iusto nemo iste delectus
-                            repudiandae et deleniti a commodi perferendis
-                            molestiae minus nostrum non?
-                        </Text>
-                    </Stack>
-                </CardBody>
-            </Card>
-            <Card
-                my={5}
-                maxW='sm'
-                rounded={'sm'}
-                border={'1px'}
-                borderColor='black'
-                boxShadow={useColorModeValue(
-                    '6px 6px 0 black',
-                    '6px 6px 0 cyan'
-                )}
-            >
-                <CardBody>
-                    <Container maxWidth='400px'>
-                        <AspectRatio ratio={4 / 3}>
-                            <ReactPlayer
-                                url={trailerUrl}
-                                width='100%'
-                                height='100%'
-                                controls={false}
-                                volume={0}
-                                muted={true}
-                                playing={false}
-                            />
-                        </AspectRatio>
-                    </Container>
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Dune (mmorpg)</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Aspernatur facere, alias laudantium
-                            voluptatibus ab veniam cum iusto nemo iste delectus
-                            repudiandae et deleniti a commodi perferendis
-                            molestiae minus nostrum non?
-                        </Text>
-                    </Stack>
-                </CardBody>
-            </Card>
-            <Card
-                my={5}
-                maxW='sm'
-                rounded={'sm'}
-                border={'1px'}
-                borderColor='black'
-                boxShadow={useColorModeValue(
-                    '6px 6px 0 black',
-                    '6px 6px 0 cyan'
-                )}
-            >
-                <CardBody>
-                    <Container maxWidth='400px'>
-                        <AspectRatio ratio={4 / 3}>
-                            <ReactPlayer
-                                url={trailerUrl}
-                                width='100%'
-                                height='100%'
-                                controls={false}
-                                volume={0}
-                                muted={true}
-                                playing={false}
-                            />
-                        </AspectRatio>
-                    </Container>
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Dune (mmorpg)</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Aspernatur facere, alias laudantium
-                            voluptatibus ab veniam cum iusto nemo iste delectus
-                            repudiandae et deleniti a commodi perferendis
-                            molestiae minus nostrum non?
-                        </Text>
-                    </Stack>
-                </CardBody>
-            </Card>
-            <Card
-                my={5}
-                maxW='sm'
-                rounded={'sm'}
-                border={'1px'}
-                borderColor='black'
-                boxShadow={useColorModeValue(
-                    '6px 6px 0 black',
-                    '6px 6px 0 cyan'
-                )}
-            >
-                <CardBody>
-                    <Container maxWidth='400px'>
-                        <AspectRatio ratio={4 / 3}>
-                            <ReactPlayer
-                                url={trailerUrl}
-                                width='100%'
-                                height='100%'
-                                controls={false}
-                                volume={0}
-                                muted={true}
-                                playing={false}
-                            />
-                        </AspectRatio>
-                    </Container>
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Dune (mmorpg)</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Aspernatur facere, alias laudantium
-                            voluptatibus ab veniam cum iusto nemo iste delectus
-                            repudiandae et deleniti a commodi perferendis
-                            molestiae minus nostrum non?
-                        </Text>
-                    </Stack>
-                </CardBody>
-            </Card>
-        </SimpleGrid>
+                <Card
+                    my={5}
+                    maxW={{
+                        base: '250px',
+                        md: '325px',
+                        lg: '400px',
+                    }}
+                    rounded={'sm'}
+                    border={'1px'}
+                    borderColor='black'
+                    boxShadow={useColorModeValue(
+                        '6px 6px 0 black',
+                        '6px 6px 0 cyan'
+                    )}
+                >
+                    <CardBody>
+                        <Container maxWidth='400px'>
+                            <AspectRatio ratio={4 / 3}>
+                                <ReactPlayer
+                                    url={trailer1}
+                                    width='100%'
+                                    height='100%'
+                                    controls={false}
+                                    volume={0}
+                                    muted={true}
+                                    playing={false}
+                                    fallback='Loading...'
+                                />
+                            </AspectRatio>
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md' letterSpacing={3}>
+                                    Lorem Ipsum
+                                </Heading>
+                            </Stack>
+                        </Container>
+                    </CardBody>
+                </Card>
+                <Card
+                    my={5}
+                    maxW={{
+                        base: '250px',
+                        md: '325px',
+                        lg: '400px',
+                    }}
+                    rounded={'sm'}
+                    border={'1px'}
+                    borderColor='black'
+                    boxShadow={useColorModeValue(
+                        '6px 6px 0 black',
+                        '6px 6px 0 cyan'
+                    )}
+                >
+                    <CardBody>
+                        <Container maxWidth='400px'>
+                            <AspectRatio ratio={4 / 3}>
+                                <ReactPlayer
+                                    url={trailer2}
+                                    width='100%'
+                                    height='100%'
+                                    controls={false}
+                                    volume={0}
+                                    muted={true}
+                                    playing={false}
+                                    fallback='Loading...'
+                                />
+                            </AspectRatio>
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md' letterSpacing={3}>
+                                    Lorem Ipsum
+                                </Heading>
+                            </Stack>
+                        </Container>
+                    </CardBody>
+                </Card>
+                <Card
+                    my={5}
+                    maxW={{
+                        base: '250px',
+                        md: '325px',
+                        lg: '400px',
+                    }}
+                    rounded={'sm'}
+                    border={'1px'}
+                    borderColor='black'
+                    boxShadow={useColorModeValue(
+                        '6px 6px 0 black',
+                        '6px 6px 0 cyan'
+                    )}
+                >
+                    <CardBody>
+                        <Container maxWidth='400px'>
+                            <AspectRatio ratio={4 / 3}>
+                                <ReactPlayer
+                                    url={trailer3}
+                                    width='100%'
+                                    height='100%'
+                                    controls={false}
+                                    volume={0}
+                                    muted={true}
+                                    playing={false}
+                                    fallback='Loading...'
+                                />
+                            </AspectRatio>
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md' letterSpacing={3}>
+                                    Lorem Ipsum
+                                </Heading>
+                            </Stack>
+                        </Container>
+                    </CardBody>
+                </Card>
+                <Card
+                    my={5}
+                    maxW={{
+                        base: '250px',
+                        md: '325px',
+                        lg: '400px',
+                    }}
+                    rounded={'sm'}
+                    border={'1px'}
+                    borderColor='black'
+                    boxShadow={useColorModeValue(
+                        '6px 6px 0 black',
+                        '6px 6px 0 cyan'
+                    )}
+                >
+                    <CardBody>
+                        <Container maxWidth='400px'>
+                            <AspectRatio ratio={4 / 3}>
+                                <ReactPlayer
+                                    url={trailer4}
+                                    width='100%'
+                                    height='100%'
+                                    controls={false}
+                                    volume={0}
+                                    muted={true}
+                                    playing={false}
+                                    fallback='Loading...'
+                                />
+                            </AspectRatio>
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md' letterSpacing={3}>
+                                    Lorem Ipsum
+                                </Heading>
+                            </Stack>
+                        </Container>
+                    </CardBody>
+                </Card>
+            </SimpleGrid>
+            <Flex mt={5}>
+                <Spacer />
+                <Link href='/' as='videos'>
+                    <Heading as='h2' size='lg' mr='100'>
+                        see all videos
+                        <ArrowForwardIcon boxSize={10} ml='5' />
+                    </Heading>
+                </Link>
+            </Flex>
+        </>
     );
 };
 
