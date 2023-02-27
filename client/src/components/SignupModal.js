24c01eb17bd056feb6698acd2ec73251e37c94f3
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-27 09:55:29 -0600

    changel handlers ion signup

--- client/src/components/SignupModal.js
+++ client/src/components/SignupModal.js
@@ -19,28 +19,34 @@ const SignupModal = ({ signupModalOpen, openSignupModal, closeSignupModal }) => 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // const handleEmailChange = (event) => {
-  //   setEmail(event.target.value);
-  // };
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
 
-  // const handlePasswordChange = (event) => {
-  //   setPassword(event.target.value);
-  // };
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   const handleSignup = () => {
-    // You can add your login logic here
-    console.log(`Sign up Email: ${email} Signup Password: ${password}`);
+    // You can add your signup logic here
+    console.table(`Signup firstName: ${firstName} Signup lastName: ${lastName} Signup Email: ${email} Signup Password: ${password}`);
     closeSignupModal();
   };
 
   return (
-<Modal
-        // initialFocusRef={initialRef}
-        // finalFocusRef={finalRef}
-        isOpen={signupModalOpen}
-        onClose={closeSignupModal}
-        isCentered
-      >
+    <Modal
+      isOpen={signupModalOpen}
+      onClose={closeSignupModal}
+      isCentered
+    >
         <ModalOverlay 
           backdropFilter='blur(10px) hue-rotate(90deg)'
         />
@@ -51,13 +57,37 @@ const SignupModal = ({ signupModalOpen, openSignupModal, closeSignupModal }) => 
             <FormControl>
               <FormLabel>First name</FormLabel>
               <Input 
-                // ref={initialRef} 
-                placeholder='First name' />
+                type="text"
+                value={firstName}
+                onChange={handleFirstNameChange}
+                 />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Last name</FormLabel>
-              <Input placeholder='Last name' />
+              <Input  
+                type="text"
+                value={lastName}
+                onChange={handleLastNameChange}
+              />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Email</FormLabel>
+              <Input  
+                type='email'
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Password</FormLabel>
+              <Input  
+                type='password'
+                value={password}
+                onChange={handlePasswordChange}
+                />
             </FormControl>
           </ModalBody>
 
@@ -68,7 +98,7 @@ const SignupModal = ({ signupModalOpen, openSignupModal, closeSignupModal }) => 
             <Button onClick={closeSignupModal}>Cancel</Button>
           </ModalFooter>
         </ModalContent>
-      </Modal>
+    </Modal>
   );
 }
 
