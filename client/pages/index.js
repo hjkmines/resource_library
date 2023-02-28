0f8514567ac227cb234ae92de703f8bca71f0f06
kristenking <kristenking0110@gmail.com>
2023-02-28 00:23:42 -0500

    changed global background color

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
 import Navbar from '../src/components/Navbar';
@@ -7,15 +7,6 @@ const Page = () => {
     return (
         <ChakraProvider>
             <Navbar />
-            {/* <div align='center'>
-                <Image
-                    src='/next.svg'
-                    alt='Next.js Logo'
-                    className={styles.thirteen}
-                    width={180}
-                    height={37}
-                />
-            </div> */}
         </ChakraProvider>
     );
 };
