b7e64b92905f11dfeb174069daacf7c80e5aaa67
Maria Delacuz <mar23iaa@gmail.com>
2023-03-22 22:43:09 -0700

    completed the event schema

---
+++ backend/models/event.js
@@ -0,0 +1,24 @@
+const mongoose = required('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  }
+}, { 
+  timestamps: true 
+})
+
+module.exports = mongoose.model('Event', EventSchema)
