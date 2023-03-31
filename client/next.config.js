5fe28ef43b4ff909c87bea21b1d88140718e79d8
Mai Vang <vmaineng@gmail.com>
2023-03-31 14:45:44 -0700

    added destination to next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/media",
+        destination: "http://localhost:5001/media",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
