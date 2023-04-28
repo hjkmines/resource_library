51f2ae1bbf69e37d4ed18c1b29a03083cec008a8
Mai Vang <vmaineng@gmail.com>
2023-04-28 13:20:08 -0700

    fetch successful

--- client/pages/allArticles/articles.js
+++ client/pages/allArticles/articles.js
@@ -1,14 +1,32 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from '@/components/Navbar';
+import Articles from '@/components/articles/[Articles]';
 import Footer from '@/components/Footer';
 
-const Page = () => {
+const Page = ( { data } ) => {
     return (
         <ChakraProvider>
             <Navbar />
+            <Articles data={data} />
             <Footer />
         </ChakraProvider>
     );
 };
 
 export default Page;
+
+//runs on server and runs before components
+export async function getServerSideProps() {
+  
+
+    const res = await fetch(`http://localhost:5001/media/`);
+    const data = await res.json();
+   
+//  console.log(data)
+  
+  
+    return {
+      props: { 
+        data: data.media },
+    };
+  };
