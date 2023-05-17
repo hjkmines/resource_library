247cad5707143d9f42ebac5373155755697e6659
Mai Vang <vmaineng@gmail.com>
2023-05-17 16:41:30 -0700

    added in index for media

--- client/pages/api/medias/index.js
+++ client/pages/api/medias/index.js
@@ -1,6 +1,12 @@
+let data = [];
 
+export default function medias(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json(data);
+  } else if (req.method === "DELETE") {
+    let { id } = req.query;
 
-
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+    let mediaDelete = data.filter((media) => media.id === id);
+    res.status(200).json(mediaDelete);
+  }
 }
